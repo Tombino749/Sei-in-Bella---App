5c5e22a29bdd5d786e39af8f3305b1eb463de972
Bovdi <alessandro.bordignon@barsanti.edu.it>
2024-04-23 11:37:06 +0200

    nuso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("com.google.firebase:firebase-auth")
     apply(plugin = "com.google.gms.google-services")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
+        classpath(libs.google.services)
     }
 }
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
     id("org.jetbrains.kotlin.android") version "2.0.0-RC1" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
